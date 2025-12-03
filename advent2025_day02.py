112c7639c4acc74e39acd106fca60c99a56336b5
Clint Olson <coandco@gmail.com>
2025-12-03 09:50:14 -0800

    Add day two solution

---
+++ advent2025_day02.py
@@ -0,0 +1,57 @@
+import time
+from math import floor
+from typing import Iterable
+
+from utils import read_data
+
+
+def repeats(startval: int, num_repeats: int) -> Iterable[int]:
+    curval = startval
+    start_len = len(str(curval))
+    while len(str(curval)) == start_len:
+        yield int(f"{curval}" * num_repeats)
+        curval += 1
+
+
+def invalids_in_range(span: range) -> Iterable[tuple[int, int]]:
+    start_str = str(span.start)
+    stop_len = len(str(span.stop - 1))
+    seen_repeats = set()
+    for num_digits in range(len(start_str), stop_len + 1):
+        # Check longer repeat lengths first to make sure double repeats don't get eaten by seen_repeats
+        for repeat_length in reversed(range(1, floor(stop_len / 2) + 1)):
+            if num_digits % repeat_length != 0 or num_digits // repeat_length < 2:
+                continue
+            repeat_start = (
+                int(start_str[:repeat_length]) if num_digits == len(start_str) else pow(10, repeat_length - 1)
+            )
+            for to_test in repeats(repeat_start, num_digits // repeat_length):
+                if to_test < span.start or to_test in seen_repeats:
+                    continue
+                if to_test >= span.stop:
+                    break
+                seen_repeats.add(to_test)
+                yield (to_test, num_digits // repeat_length)
+
+
+def double_repeats(spans: list[range]) -> Iterable[int]:
+    for span in spans:
+        yield from (x[0] for x in invalids_in_range(span) if x[1] == 2)
+
+
+def all_repeats(spans: list[range]) -> Iterable[int]:
+    for span in spans:
+        yield from (x[0] for x in invalids_in_range(span))
+
+
+def main():
+    range_strs = [x.split("-") for x in read_data().split(",")]
+    ranges = [range(int(x[0]), int(x[1]) + 1) for x in range_strs]
+    print(f"Part one: {sum(double_repeats(ranges))}")
+    print(f"Part two: {sum(all_repeats(ranges))}")
+
+
+if __name__ == "__main__":
+    start = time.monotonic()
+    main()
+    print(f"Time: {time.monotonic()-start}")
