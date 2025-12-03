e7588d63eb11361f9746a5004d662803e921a911
Clint Olson <coandco@gmail.com>
2025-12-03 10:13:17 -0800

    Comments and cleanup for day 2

--- advent2025_day02.py
+++ advent2025_day02.py
@@ -17,21 +17,23 @@ def invalids_in_range(span: range) -> Iterable[tuple[int, int]]:
     start_str = str(span.start)
     stop_len = len(str(span.stop - 1))
     seen_repeats = set()
+    # This checks all the possible lengths, e.g. it checks 2, 3, and 4 if the range is 10-1000
     for num_digits in range(len(start_str), stop_len + 1):
         # Check longer repeat lengths first to make sure double repeats don't get eaten by seen_repeats
         for repeat_length in reversed(range(1, floor(stop_len / 2) + 1)):
+            # We only test lengths that divide evenly into the number of digits, and there has to be at least 2 repeats
             if num_digits % repeat_length != 0 or num_digits // repeat_length < 2:
                 continue
-            repeat_start = (
-                int(start_str[:repeat_length]) if num_digits == len(start_str) else pow(10, repeat_length - 1)
-            )
-            for to_test in repeats(repeat_start, num_digits // repeat_length):
+            # For the smallest number of digits we can start testing for repeats based on the start of the range
+            # If we're at a larger number of digits, we start where it rolls over, e.g. 10000 for a 5-length number
+            test_start = int(start_str[:repeat_length]) if num_digits == len(start_str) else pow(10, repeat_length - 1)
+            for to_test in repeats(test_start, num_digits // repeat_length):
                 if to_test < span.start or to_test in seen_repeats:
                     continue
                 if to_test >= span.stop:
                     break
                 seen_repeats.add(to_test)
-                yield (to_test, num_digits // repeat_length)
+                yield to_test, num_digits // repeat_length
 
 
 def double_repeats(spans: list[range]) -> Iterable[int]:
