4dac82dff11782afaedd8eff6842fe7c8dfc0d2b
Clint Olson <coandco@gmail.com>
2025-12-04 15:08:58 -0800

    Improve day 4 speed by another order of magnitude

--- advent2025_day04.py
+++ advent2025_day04.py
@@ -5,38 +5,37 @@ from utils import BaseCoord as Coord
 from utils import read_data
 
 
-def calc_neighbors(grid: dict[Coord, int]):
+def calc_neighbors(grid: dict[Coord, set[Coord]]):
     for coord in grid:
-        grid[coord] = sum(x in grid for x in coord.neighbors())
+        grid[coord] = {x for x in coord.neighbors() if x in grid}
 
 
-def recalc_neighbors(grid: dict[Coord, int], removed: Iterable[Coord]):
+def recalc_neighbors(grid: dict[Coord, set[Coord]], removed: Iterable[Coord]):
     for pos in removed:
-        for neighbor in pos.neighbors():
-            if neighbor in grid:
-                grid[neighbor] = max(grid[neighbor] - 1, 0)
+        for neighbor in grid[pos]:
+            grid[neighbor].discard(pos)
 
 
-def read_grid(raw_grid: str) -> dict[Coord, int]:
+def read_grid(raw_grid: str) -> dict[Coord, set[Coord]]:
     grid = {}
     for y, line in enumerate(raw_grid.splitlines()):
         for x, char in enumerate(line):
             if char == "@":
-                grid[Coord(x=x, y=y)] = 0
+                grid[Coord(x=x, y=y)] = set()
     calc_neighbors(grid)
     return grid
 
 
 def main():
     grid = read_grid(read_data())
-    can_remove = [k for k, v in grid.items() if v < 4]
+    can_remove = [k for k, v in grid.items() if len(v) < 4]
     print(f"Part one: {len(can_remove)}")
     total_removed = 0
     while can_remove:
         total_removed += len(can_remove)
-        [grid.pop(x) for x in can_remove]
         recalc_neighbors(grid, can_remove)
-        can_remove = [k for k, v in grid.items() if v < 4]
+        [grid.pop(x) for x in can_remove]
+        can_remove = [k for k, v in grid.items() if len(v) < 4]
     print(f"Part two: {total_removed}")
 
 
